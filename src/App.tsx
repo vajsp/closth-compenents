1bc8be35d0ca93e42cfa996503c18f30bbc19865
ma <442026460@qq.com>
2021-09-02 15:01:56 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 // import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 import "./index.scss";
+import { Button } from "./components/View3d/index";
 
 function App() {
-  console.log(123455);
+  // console.log(123455);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button></Button>
     </div>
   );
 }
---
+++ src/components/View3d/button.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Button: FC = () => {
+  console.log(1234);
+  return <div>测试组件</div>;
+};
+export { Button };
